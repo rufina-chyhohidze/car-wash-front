a17b15372e7a4920f9bc77d4c8f17d6ce37f68a8
rufinachyhohidze <rufina.chyhohidze@student.kdg.be>
2026-03-25 19:00:21 +0100

    update resources

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -158,7 +158,7 @@ export default function Project() {
                                 }}
                             >
                                 <iframe
-                                    src="/docs/article(2).pdf"
+                                    src="/docs/Article-2.pdf"
                                     width="100%"
                                     height="100%"
                                     style={{ border: "none" }}
@@ -169,7 +169,7 @@ export default function Project() {
                             <Button
                                 variant="contained"
                                 startIcon={<DescriptionIcon />}
-                                href="/docs/article(2).pdf"
+                                href="/docs/Article-2.pdf"
                                 target="_blank"
                                 sx={{
                                     background: "#22d3ee",
